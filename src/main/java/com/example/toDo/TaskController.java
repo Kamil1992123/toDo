344741336f2da3ba1f69014d1f0b55368aba4954
kamil1992123 <kamil123_1992@o2.pl>
2018-08-11 17:17:50 +0200

    tasks list view

--- src/main/java/com/example/toDo/TaskController.java
+++ src/main/java/com/example/toDo/TaskController.java
@@ -12,7 +12,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class TaskController {
 
-    @ResponseBody // Nie zwracaj html'a tylko Stringa
+    private TaskDao taskDao = new StaticDao();
+
+
+//    @ResponseBody // Nie zwracaj html'a tylko Stringa
     @PostMapping("/tasks")
     public String create(@RequestParam String name,
                          @RequestParam String description,
@@ -21,11 +24,35 @@ public class TaskController {
     ){
         Task task = new Task(name,description,finished);
         modelMap.put("task",task);
-        return "added" + task;
+        taskDao.addTask(task);
+        return "redirect:/tasks";
+        // przejdź do scieżki /tasks z metodą http get
     }
+
+    @GetMapping("/tasks")
+    public String index(ModelMap modelMap){
+
+        modelMap.put("tasks",taskDao.findAll());
+        return "index";
+    }
+
+    @GetMapping("/finished")
+    public String finished(ModelMap modelMap){
+        modelMap.put("tasks",taskDao.findByStatus(true));
+        return "index";
+    }
+
+    @GetMapping("/unfinished")
+    public String unfinished(ModelMap modelMap){
+        modelMap.put("tasks",taskDao.findByStatus(false));
+        return "index";
+    }
+
 
     @GetMapping("/")
     public String add(){
         return "add";
     }
+
+
 }
